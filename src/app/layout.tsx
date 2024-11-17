bc35e845cdc2970d5713e4f13e86503c5fe96e51
emre1n <emre1n@protonmail.com>
2024-11-17 22:41:57 +0300

    add vercel analytics & layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <div className="min-h-screen flex flex-col justify-between p-8 pt-0 md:pt-8 bg-white text-gray-900">
+          <main className="w-full space-y-6">{children}</main>
+          <Footer />
+          <Analytics />
+        </div>
+      </body>
     </html>
   );
 }
